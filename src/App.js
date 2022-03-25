aea28f1ceaab30d095fdb29ecf9e79c4ced8c552
priya <blpriya1503@gmail.com>
2022-03-25 20:35:19 +0530

    added api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
+import axios from "axios";
 
 function App() {
+  
+  const[text,setText] = useState("");
+  const[sentiment,setsentiment] = useState("");
+
+  const clickHandler = async () =>{
+    const obj={text :text};
+    const res= await axios.post("/api/NLP-trigger",obj);
+    setsentiment(res.data);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>Sentiment Analysis of Paragraph</h3>
+      <label for="inputText">Enter input Text</label>
+      <br></br>
+      <textarea id="inputText" rows="10" cols="50" value={text} onChange={(e) => setText(e.target.value)}></textarea>
+      <br></br>
+      <br></br>
+      <button id="button" onClick={clickHandler}>Analyze</button>
+      <h1 id="sentiment">{sentiment}</h1>
     </div>
   );
 }
